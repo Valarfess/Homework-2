4e918d2cbce902ebedf3ca3d40c46ae01da5e776
valarfess <valarfess@gmail.com>
2021-02-15 09:35:45 +0300

    Домашка 2

---
+++ src/academy/belhard/Main.java
@@ -0,0 +1,27 @@
+package academy.belhard;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int yearOfBirthday = 1986;
+        int monthOfBirthday = 9;
+        int dayOfBirthday = 25;
+        int sum = yearOfBirthday + monthOfBirthday + dayOfBirthday;
+        boolean monthBiggerDay;
+        char [] name = {'М','и','х','а','и','л'};
+        double myAge = 34.0;
+        double partYear = 6.0/12.0;
+        double result = myAge + partYear;
+        System.out.println("Сумма моего года, месяца и дня рождения: " + sum);
+        if (dayOfBirthday < monthOfBirthday){System.out.println("Месяц моего рождения больше даты рождения: True");}
+            else {System.out.println("Месяц моего рождения больше даты рождения: False");}
+        System.out.println("Массив с моим именем: " + name);
+        System.out.println("Мне - " + result + " лет");
+
+
+
+
+
+        // write your code here
+    }
+}
